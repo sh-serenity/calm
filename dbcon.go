278349a01c0749a6197da4a81953a55d2734283b
Alexey <int@shushik.net>
2020-04-04 09:47:59 +0300

    mysql

--- dbcon.go
+++ dbcon.go
@@ -9,7 +9,7 @@ func dbConnect() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "cerber"
 	dbPass := "OU-Shit"
-	dbHost := "calm-mysql"
+	dbHost := "mysql"
 	dbPort := "3306"
 	dbName := "shwork"
 	db, err := sql.Open(dbDriver, dbUser +":"+ dbPass +"@tcp("+ dbHost +":"+ dbPort +")/"+ dbName +"?charset=utf8")
